46a00793f385a3576306f4dd305aa6ad55038273
Jromary <julienromary@yahoo.fr>
2019-02-23 15:05:25 +0100

    debut des ameliorations

--- README.md
+++ README.md
@@ -4,23 +4,27 @@
 Le but est de réaliser un logiciel permettant de diminuer la taille d’une 
 image en supprimant progressivement des colonnes “inutiles”, comme présenté en TD.
 Le travail est en deux parties :
+
 - La première partie consiste à produire un logiciel qui enlève 50 pixels par ligne, en enlevant colonne
 par colonne.
-- La seconde partie consiste à implémenter un parcours en profondeur sans utiliser de récursivité.
-- Chacune des deux parties peut être accompagnée d’améliorations, décrites dans ce document.
 
+- La seconde partie consiste à implémenter un parcours 
+en profondeur sans utiliser de récursivité.
+
+# 1er Partie
 ## Utilisation du projet
 
 Un executable .jar nommé projet.jar peut étre utiliser pour éxécuter le programme, 
 celui-ci prend 3 paramétres : 
-* Le fichier .pgm d'entré (doit ce trouver dans le même dossier que l'executable)
-* Le nombre de ligne a enlever (entre 0 et la largeur max de l'image)
-* Lee fichier de sortie de l'image générer par le programme
+
+- Le fichier .pgm d'entré (doit ce trouver dans le même dossier que l'executable)
+- Le nombre de ligne a enlever (entre 0 et la largeur max de l'image)
+- Le fichier de sortie de l'image générer par le programme
 Si l'executable ne fonctionne pas, dans le src/modele/ se trouve la classe main qui elle 
 aussi prend ces 3 paramétres.
 
 ## Modification éffectuées
-* Mise en public de la classe Edge (utilisation externe au package)
+- Mise en public de la classe Edge (utilisation externe au package)
 
 ## Image ajouté
 Ajout de l'image venus2.ascii.pgm (Image source dans src/ressources/)
@@ -43,6 +47,10 @@ obtenir.
 a la sortie du neud choisi on retest toutes les arétes dans l'ordre, ce qui fait qu'on traite
 trop de fois toutes les arrétes. Exemple: 1 sommet relié a 99 autre, la première arréte testé
 sera tester 99 fois, ce qui est beaucoup trop.
+
+## Amelorations effectué
+* mise en place du graph implicite
+* mise en plase de la fonction d'energie avant dans le graph implicite
 
 ## Sources
 https://people.sc.fsu.edu/~jburkardt/data/pgma/pgma.html
--- projet/src/modele/graph/GraphImplicit.java
+++ projet/src/modele/graph/GraphImplicit.java
@@ -3,91 +3,130 @@ package modele.graph;
 import java.util.ArrayList;
 
 public class GraphImplicit extends Graph{
-	private int h;
-	private int w;
-	int[][] interest;
-	int N;
+    private int h;
+    private int w;
+    int[][] interest;
+    int N;
 
-	@SuppressWarnings("unchecked")
-	public GraphImplicit(int N){
-		this.N = N;
-	}
+    @SuppressWarnings("unchecked")
+    public GraphImplicit(int N){
+        this.N = N;
+    }
 
-	public GraphImplicit(int[][] interest, int w, int h){
-		this(w*h + 2);
-		this.w = w;
-		this.h = h;
-		this.interest = interest;
-	}
+    public GraphImplicit(int[][] interest, int w, int h){
+        this(w*h + 2);
+        this.w = w;
+        this.h = h;
+        this.interest = interest;
+    }
 
-	public int vertices(){
-		return N;
-	}
+    public int vertices(){
+        return N;
+    }
 
-	@Override
-	public Iterable<Edge> next(int v) {
-		ArrayList<Edge> edges = new ArrayList();
-		//on est au dernier sommet (t)
-		if (v == N - 1){
-			return edges;
-		}
-		//on est au premier sommet (s)
-		if (v == N - 2){
-			for (int i = 0; i < w; i++) {
-				edges.add(new Edge(N-2, i, 0));
-			}
-			return edges;
-		}
+    @Override
+    public Iterable<Edge> next(int v) {
+        ArrayList<Edge> edges = new ArrayList();
+        //on est au dernier sommet (t)
+        if (v == N - 1){
+            return edges;
+        }
+        //on est au premier sommet (s)
+        if (v == N - 2){
+            for (int i = 0; i < w; i++) {
+                edges.add(new Edge(N-2, i, 0));
+            }
+            return edges;
+        }
 
-		int posX;
-		int posY;
+        int posX;
+        int posY;
 
-		posX = v%w;
-		posY = v/w;
+        posX = v%w;
+        posY = v/w;
 
-		if (posY == h-1){
-			edges.add(new Edge(v, N-1, interest[posY][posX]));
-			return edges;
-		}
+        if (posY == h-1){
+            if (posX == 0){
+                edges.add(new Edge(v, N-1, interest[posY][posX+1]));
+                return edges;
+            }
+            if (posX == w-1){
+                edges.add(new Edge(v, N-1, interest[posY][posX-1]));
+                return edges;
+            }
+            edges.add(new Edge(v, N-1, Math.abs(interest[posY][posX-1] - interest[posY][posX+1])));
+            return edges;
+        }
 
-		//on max est min pour pas depasser a droite ou a gauche
-		edges.add(new Edge(v,(posY+1)*w + Integer.max(0,posX-1), interest[posY][posX]));
-		edges.add(new Edge(v,(posY+1)*w + posX, interest[posY][posX]));
-		edges.add(new Edge(v,(posY+1)*w + Integer.min(w-1,posX+1), interest[posY][posX]));
 
-		return edges;
-	}
+        if (posX == 0){
+            edges.add(new Edge(v, (posY+1)*w + posX+1, Math.abs(interest[posY][posX+1] - interest[posY+1][posX])));
+            edges.add(new Edge(v, (posY+1)*w + posX, interest[posY][posX+1]));
+            return edges;
+        }
+        if (posX == w-1){
+            edges.add(new Edge(v, (posY+1)*w + posX-1, Math.abs(interest[posY][posX-1] - interest[posY+1][posX])));
+            edges.add(new Edge(v, (posY+1)*w + posX, interest[posY][posX-1]));
+            return edges;
+        }
 
-	@Override
-	public Iterable<Edge> prev(int v) {
-		ArrayList<Edge> edges = new ArrayList();
+        edges.add(new Edge(v,(posY+1)*w + posX-1, Math.abs(interest[posY+1][posX] - interest[posY][posX-1])));
+        edges.add(new Edge(v,(posY+1)*w + posX, Math.abs(interest[posY][posX+1] - interest[posY][posX-1])));
+        edges.add(new Edge(v,(posY+1)*w + posX+1, Math.abs(interest[posY+1][posX] - interest[posY][posX+1])));
 
-		if (v == N-1){
-			for (int i = 0; i < w; i++) {
-				edges.add(new Edge((h-1)*w + i,N-1, interest[(h-1)][i]));
-			}
-			return edges;
-		}
+        return edges;
+    }
 
-		if (v == N-2){
-			return edges;
-		}
+    @Override
+    public Iterable<Edge> prev(int v) {
+        ArrayList<Edge> edges = new ArrayList();
 
-		int posX;
-		int posY;
+        if (v == N-1){
+            for (int i = 0; i < w; i++) {
+                if (i == 0){
+                    edges.add(new Edge((h-1)*w + i,N-1, interest[(h-1)][i+1]));
+                }else{
+                    if (i == w-1){
+                        edges.add(new Edge((h-1)*w + i,N-1, interest[(h-1)][i-1]));
+                    }else {
+                        edges.add(new Edge((h-1)*w + i,N-1, Math.abs(interest[(h-1)][i+1] - interest[(h-1)][i-1])));
+                    }
+                }
+            }
+            return edges;
+        }
 
-		posX = v%w;
-		posY = v/w;
+        if (v == N-2){
+            return edges;
+        }
 
-		if (posY == 0){
-			edges.add(new Edge(N-2, v, 0));
-			return edges;
-		}
+        int posX;
+        int posY;
 
-		edges.add(new Edge((posY-1)*w + Integer.max(0, posX -1), v, interest[posY-1][Integer.max(0, posX -1)]));
-		edges.add(new Edge((posY-1)*w + posX, v, interest[posY-1][posX]));
-		edges.add(new Edge((posY-1)*w + Integer.max(w-1, posX+1), v, interest[posY-1][Integer.min(w-1, posX+1)]));
+        posX = v%w;
+        posY = v/w;
 
-		return edges;
-	}
+        if (posY == 0){
+            edges.add(new Edge(N-2, v, 0));
+            return edges;
+        }
+
+        if (posX == 0){
+            edges.add(new Edge((posY-1)*w + posX+1, v, Math.abs(interest[posY-1][posX] - interest[posY][posX+1])));
+            edges.add(new Edge((posY-1)*w + posX, v, interest[posY-1][posX+1]));
+            return edges;
+        }
+        if (posX == w-1){
+            edges.add(new Edge(v, (posY-1)*w + posX-1, Math.abs(interest[posY][posX-1] - interest[posY-1][posX])));
+            edges.add(new Edge(v, (posY-1)*w + posX, interest[posY-1][posX-1]));
+            return edges;
+        }
+
+
+        edges.add(new Edge((posY-1)*w + posX-1, v, Math.abs(interest[posY][posX-1] - interest[posY-1][posX])));
+        edges.add(new Edge((posY-1)*w + posX, v, Math.abs(interest[posY-1][posX-1] - interest[posY-1][posX+1])));
+        edges.add(new Edge((posY-1)*w + posX+1, v, Math.abs(interest[posY-1][posX] - interest[posY][posX+1])));
+
+        return edges;
+    }
 }
